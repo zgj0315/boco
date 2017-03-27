95b7fa0035a75a5f4b9f3a7f40d172fbe6439bba
Zhao Guangjian <zgj0315@gmail.com>
2017-03-27 16:36:48 +0800

    fix mro

--- mroSpark/src/main/scala/cn/com/boco/mro/MroXmlGzRead.scala
+++ mroSpark/src/main/scala/cn/com/boco/mro/MroXmlGzRead.scala
@@ -18,23 +18,39 @@ object MroXmlGzRead {
     val mroXmlRdd = sc.textFile("/user/boco_zhaogj/mro_xml/")
     //mroXmlRdd.saveAsTextFile("/user/boco_zhaogj/output")
 
-    var strTitle: String = null
     var strObject: String = null
+    var astrTitle: Array[String] = null
+
     mroXmlRdd.map { x => {
       var strLine = x.trim()
       if (strLine.startsWith("<v>") && strLine.endsWith("</v>")) {
         strLine = strLine.substring(3)
         strLine = strLine.substring(0, strLine.length - 4)
-        strTitle + strObject + strLine
+        val astrValue = strLine.split(" ")
+        val sbOutput: StringBuffer = new StringBuffer()
+        sbOutput.append(strObject)
+        for (i <- 0 until astrTitle.length) {
+          sbOutput.append(", \"")
+          sbOutput.append(astrTitle(i))
+          sbOutput.append("\":\"")
+          sbOutput.append(astrValue(i))
+          sbOutput.append("\"")
+        }
+        sbOutput.append("}\n")
+        sbOutput.toString()
       } else if (strLine.startsWith("<object ") && strLine.endsWith(">")) {
         strLine = strLine.substring(8)
         strLine = strLine.substring(0, strLine.length - 1)
-        strObject = strLine
-      }
-      else if (strLine.startsWith("<smr>") && strLine.endsWith("</smr>")) {
+        strObject = "{\"" + strLine.replaceAll("\" ", "\", \"").replaceAll("=\"", "\":\"")
+        ""
+      } else if (strLine.startsWith("<smr>") && strLine.endsWith("</smr>")) {
         strLine = strLine.substring(5)
         strLine = strLine.substring(0, strLine.length - 6)
-        strTitle = strLine
+        strLine = strLine.replaceAll("\\.", "\\_")
+        astrTitle = strLine.split(" ")
+        ""
+      } else {
+        ""
       }
     }
     }.saveAsTextFile("/user/boco_zhaogj/output")
